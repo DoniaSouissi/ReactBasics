923cebb2da22f0548ca6be26399991dfb41a5caf
DoniaSouissi <souissidonia13@gmail.com>
2024-02-12 13:32:55 +0000

    reactlab

---
+++ reactlab/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return ( 
+    <div className="App"> 
+      
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer /> 
+    </div> 
+  ); 
+}
+
+
+ 
+export default App;
